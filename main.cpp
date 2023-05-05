b77c06992eaf5379de49ae8e76302fca338a9f81
WertiPL <wiktor.rostkowski@hotmail.com>
2023-05-05 23:21:11 +0200

    Physics

--- main.cpp
+++ main.cpp
@@ -1,57 +1,168 @@
-#include <SDL.h>
-#include <chrono>
-#include <cstdint>
 #include <iostream>
 #include <memory>
-#include <set>
-#include <stdexcept>
-#include <string>
-#include <thread>
-#include <tuple>
-#include <vector>
+#include <array>
+#include <cmath>
 
-// check for errors
-#define errcheck(e)                                                            \
-  {                                                                            \
-    if (e) {                                                                   \
-      cout << SDL_GetError() << endl;                                          \
-      SDL_Quit();                                                              \
-      return -1;                                                               \
-    }                                                                          \
-  }
+#define SDL_MAIN_HANDLED
 
-int main(int , char **) {
-    using namespace std;
-    using namespace std::chrono;
-    const int width = 640;
-    const int height = 480;
+#include <SDL.h>
 
-    errcheck(SDL_Init(SDL_INIT_VIDEO) != 0);
+const int TICK_TIME = 33;
 
-    SDL_Window *window = SDL_CreateWindow(
-            "My Next Superawesome Game", SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
-    errcheck(window == nullptr);
+std::shared_ptr<SDL_Texture> load_texture(SDL_Renderer *renderer, std::string fname) {
+    SDL_Surface *bmp = SDL_LoadBMP(("assets/" + fname).c_str());
+    if (!bmp) {
+        throw std::invalid_argument("Could not load bmp");
+    }
+    SDL_SetColorKey(bmp, SDL_TRUE, 0x0ff00ff);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, bmp);
+    if (!texture) {
+        throw std::invalid_argument("Could not create texture");
+    }
+    SDL_FreeSurface(bmp);
+    return std::shared_ptr<SDL_Texture>(texture, [](SDL_Texture *tex) {
+        std::cout << "Texture destroyed!" << std::endl;
+        SDL_DestroyTexture(tex);
+    });
+}
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(
-            window, -1, SDL_RENDERER_ACCELERATED); // SDL_RENDERER_PRESENTVSYNC
-    errcheck(renderer == nullptr);
+std::pair<int, int> get_texture_w_h(std::shared_ptr<SDL_Texture> texture) {
+    int w, h;
+    SDL_QueryTexture(texture.get(), nullptr, nullptr, &w, &h);
+    return {w, h};
+}
 
-    //auto dt = 15ms;
-    milliseconds dt(15);
+SDL_Rect get_texture_rect(std::shared_ptr<SDL_Texture> texture) {
+    auto [w, h] = get_texture_w_h(texture);
+    return {0, 0, w, h};
+}
 
-    steady_clock::time_point current_time = steady_clock::now(); // remember current time
-    for (bool game_active = true; game_active;) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) { // check if there are some events
-            if (event.type == SDL_QUIT)
-                game_active = false;
+using vec2d = std::array<double, 2>;
+
+vec2d operator+(vec2d a, vec2d b) {
+    return {a[0] + b[0], a[1] + b[1]};
+}
+
+vec2d operator-(vec2d a, vec2d b) {
+    return {a[0] - b[0], a[1] - b[1]};
+}
+
+vec2d operator*(vec2d a, double b) {
+    return {a[0] * b, a[1] * b};
+}
+
+vec2d operator/(vec2d a, double b) {
+    return a * (1.0 / b);
+}
+double len(vec2d v) {
+    return std::sqrt(v[0]*v[0] + v[1]*v[1]);
+}
+class player_c {
+public:
+    double angle;
+    vec2d position;
+    vec2d velocity;
+    vec2d acceleration;
+
+    player_c next_state(double dt_ms) {
+        double dt = dt_ms / 1000.0;
+        player_c next = *this;
+        const double C = 0.1;
+        vec2d friction = {0.0,0.0};
+        if (len(velocity) > 0) {
+            friction = velocity*len(velocity)*C;
+        }
+        auto a = acceleration - friction;
+        next.position = position + velocity * dt + (a * dt * dt) / 2;
+        next.velocity = velocity + a * dt;
+        next.acceleration = a;
+
+
+
+
+        return next;
+    }
+};
+
+vec2d angle_to_vector(double angle) {
+    return {std::cos(angle), std::sin(angle)};
+}
+
+vec2d acceleration_vector_from_keyboard_and_player(const player_c &player) {
+    auto *keyboard_state = SDL_GetKeyboardState(nullptr);
+    vec2d forward_vec = angle_to_vector(player.angle);
+    vec2d acceleration = {0, 0};
+    if (keyboard_state[SDL_SCANCODE_UP]) {
+        acceleration = acceleration + forward_vec;
+    }
+    if (keyboard_state[SDL_SCANCODE_DOWN]) {
+        acceleration = acceleration - forward_vec;
+    }
+    return acceleration*200.0;
+}
+
+double angle_from_keyboard_and_player(const player_c &player) {
+    auto *keyboard_state = SDL_GetKeyboardState(nullptr);
+    double angle = player.angle;
+    if (keyboard_state[SDL_SCANCODE_LEFT]) angle = angle - M_PI / 10.0;
+    if (keyboard_state[SDL_SCANCODE_RIGHT]) angle = angle + M_PI / 10.0;
+    return angle;
+}
+
+void play_the_game(SDL_Renderer *renderer) {
+    auto player_texture = load_texture(renderer, "player1.bmp");
+    SDL_Rect player_rect = get_texture_rect(player_texture);
+    player_c player = {0, {320.0, 200.0}};
+    int gaming = true;
+    auto prev_tick = SDL_GetTicks();
+    while (gaming) {
+        SDL_Event e;
+
+        while (SDL_PollEvent(&e) != 0) {
+            switch (e.type) {
+                case SDL_QUIT:
+                    gaming = false;
+                    break;
+                case SDL_KEYDOWN:
+                    if (e.key.keysym.sym == SDLK_q) gaming = false;
+                    break;
+            }
         }
 
-        SDL_RenderPresent(renderer); // draw frame to screen
+        player.acceleration = acceleration_vector_from_keyboard_and_player(player);
+        player.angle = angle_from_keyboard_and_player(player);
+        player = player.next_state(TICK_TIME);
 
-        this_thread::sleep_until(current_time = current_time + dt);
+
+        SDL_SetRenderDrawColor(renderer, 1, 40, 128, 255);
+        SDL_RenderClear(renderer);
+
+        //SDL_RenderCopy(renderer, player_texture.get(), nullptr, &player_rect);
+        {
+            auto rect = player_rect;
+
+            rect.x = player.position[0] - rect.w / 2;
+            rect.y = player.position[1] - rect.h / 2;
+            SDL_RenderCopyEx(renderer, player_texture.get(),
+                             nullptr, &rect, 180.0 * player.angle / M_PI,
+                             nullptr, SDL_FLIP_NONE);
+        }
+        SDL_RenderPresent(renderer);
+        auto current_tick = SDL_GetTicks();
+        SDL_Delay(TICK_TIME - (current_tick - prev_tick));
+        prev_tick += TICK_TIME;
     }
+}
+
+int main() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(640, 480,
+                                SDL_WINDOW_SHOWN,
+                                &window, &renderer);
+
+    play_the_game(renderer);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
